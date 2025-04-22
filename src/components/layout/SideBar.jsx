a11d6b67cf428c99dc82614612c5da5683a8797d
Hoang PV1 <hoangpv1@bsscommerce.com>
2025-04-22 14:06:16 +0700

    Update SideBar

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -53,38 +53,38 @@ const SideBar = () => {
 
   return (
     <>
-      <div class="left-side-menu">
-        <a href="/dashboard" class="logo text-center logo-light">
-          <span class="logo-lg">
+      <div className="left-side-menu">
+        <a href="/dashboard" className="logo text-center logo-light">
+          <span className="logo-lg">
             <img src="assets/images/logo_hostel.png" alt="" height="48" />
           </span>
-          <span class="logo-sm">
+          <span className="logo-sm">
             <img src="assets/images/logo_hostel_sm.png" alt="" height="48" />
           </span>
         </a>
 
-        <div class="h-100 mt-3" id="left-side-menu-container" data-simplebar>
-          <ul class="metismenu side-nav">
+        <div
+          className="h-100 mt-3"
+          id="left-side-menu-container"
+          data-simplebar
+        >
+          <ul className="metismenu side-nav">
             {filteredMenuItems.map((item) => (
-              <li class="side-nav-item" key={item.path}>
-                <NavLink
-                  to={item.path}
-                  className="side-nav-link"
-                  activeClassName="active"
-                >
+              <li className="side-nav-item" key={item.path}>
+                <NavLink to={item.path} className="side-nav-link">
                   <i className="dripicons-chevron-right"></i>
                   <span className="h4 font-weight-normal">{item.label}</span>
                 </NavLink>
               </li>
             ))}
-            {/* <li class="side-nav-item">
-              <a href="dashboard" class="side-nav-link">
-                <i class="uil-calender"></i>
+            {/* <li className="side-nav-item">
+              <a href="dashboard" className="side-nav-link">
+                <i className="uil-calender"></i>
                 <span> Dashboard </span>
               </a>
             </li> */}
           </ul>
-          <div class="clearfix"></div>
+          <div className="clearfix"></div>
         </div>
       </div>
     </>
